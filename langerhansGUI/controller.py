49de9f09fe6e619740a201a1da0f7e7087484804
Jan <jan.zmazek@gmail.com>
2021-03-02 14:56:39 +0100

    PEP8 edits.

--- langerhansGUI/controller.py
+++ langerhansGUI/controller.py
@@ -20,13 +20,13 @@ class Controller(object):
         self.current_number = 0
         self.current_stage = 0
 
-# ---------------------------- Menu click methods ---------------------------- #
+# ---------------------------- Menu click methods --------------------------- #
     def import_data(self):
         filename = self.view.open_file()
         if filename is None:
             return
         try:
-            series = np.loadtxt(filename)[:-1,:]
+            series = np.loadtxt(filename)[:-1, :]
             self.data.import_data(series)
             self.current_stage = "imported"
             self.draw_fig()
@@ -43,6 +43,7 @@ class Controller(object):
             try:
                 settings = yaml.safe_load(stream)
             except yaml.YAMLError as exc:
+                print(exc)
                 raise(ValueError("Could not open settings file."))
         try:
             self.data.import_settings(settings)
@@ -72,8 +73,8 @@ class Controller(object):
         try:
             with open(filename, 'rb') as input:
                 self.data = pickle.load(input)
-        except:
-            print("Unsuccessful.")
+        except Exception as exc:
+            print("Unsuccessful: {}.".format(exc))
         self.current_stage = "imported"
         self.draw_fig()
 
@@ -90,7 +91,9 @@ class Controller(object):
         if filename is None:
             return
         with open(filename, "w") as outfile:
-            yaml.dump(self.data.get_settings(), outfile, default_flow_style=False)
+            yaml.dump(self.data.get_settings(), outfile,
+                      default_flow_style=False
+                      )
 
     def save_image(self):
         if self.current_stage == 0:
@@ -98,7 +101,7 @@ class Controller(object):
         file = self.view.save_as("pdf")
         if file is None:
             return
-        fig = self.__get_fig()
+        self.__get_fig()
         plt.savefig(file)
 
     def save_eventplot(self):
@@ -107,7 +110,7 @@ class Controller(object):
         file = self.view.save_as("pdf")
         if file is None:
             return
-        fig = self.data.plot_events()
+        self.data.plot_events()
         plt.savefig(file)
 
     def save_excluded(self):
@@ -129,12 +132,13 @@ class Controller(object):
         with open(filename, "wb") as output:
             pickle.dump(self.data, output, pickle.HIGHEST_PROTOCOL)
 
-# --------------------------- Button click methods --------------------------- #
+# --------------------------- Button click methods -------------------------- #
 
     def filter_click(self):
         if self.current_stage == 0:
             return
-        if self.data.get_filtered_slow() is not False or self.data.get_filtered_fast() is not False:
+        if self.data.get_filtered_slow() is not False or \
+                self.data.get_filtered_fast() is not False:
             self.current_stage = "filtered"
             self.draw_fig()
         else:
@@ -174,14 +178,17 @@ class Controller(object):
     def binarize_click(self):
         if self.current_stage == 0:
             return
-        if self.data.get_binarized_slow() is not False or self.data.get_binarized_fast() is not False:
+        if self.data.get_binarized_slow() is not False or \
+                self.data.get_binarized_fast() is not False:
             self.current_stage = "binarized"
             self.draw_fig()
         else:
             try:
                 checkpoint = 0.1
-                for (i, j) in zip(self.data.binarize_fast(), self.data.binarize_slow()):
-                    print(i)
+                for (i, j) in zip(self.data.binarize_fast(),
+                                  self.data.binarize_slow()
+                                  ):
+                    print(i, j)
                     self.view.update_progressbar(i*100)
                     if i > checkpoint:
                         checkpoint += 0.1
@@ -256,14 +263,18 @@ class Controller(object):
         if self.current_stage == "imported":
             fig, (ax1, ax2) = plt.subplots(nrows=2)
             self.data.plot(ax1, self.current_number, plots=("mean"))
-            self.data.plot(ax2, self.current_number, plots=("raw"), protocol=False)
+            self.data.plot(
+                ax2, self.current_number, plots=("raw"), protocol=False
+                )
             return fig
         elif self.current_stage == "filtered":
             fig, (ax1, ax2) = plt.subplots(2)
             fig.suptitle("Filtered data")
             self.data.plot(ax1, self.current_number, plots=("raw"))
             ax1.set_xlabel(None)
-            self.data.plot(ax2, self.current_number, plots=("fast"), protocol=False)
+            self.data.plot(
+                ax2, self.current_number, plots=("fast"), protocol=False
+                )
             return fig
         elif self.current_stage == "distributions":
             fig = plt.figure(constrained_layout=True)
@@ -274,14 +285,19 @@ class Controller(object):
             ax12.set_title("Distribution of post-stimulatory signal")
             ax2 = fig.add_subplot(gs[1, :])
             self.data.plot_distributions(ax11, ax12, self.current_number)
-            self.data.plot(ax2, self.current_number, plots=("raw, fast"), noise=True)
+            self.data.plot(
+                ax2, self.current_number, plots=("raw, fast"), noise=True
+                )
             ax2.legend()
             return fig
         elif self.current_stage == "binarized":
             fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
             fig.suptitle("Binarized data")
             self.data.plot(ax1, self.current_number, plots=("raw"))
-            self.data.plot(ax2, self.current_number, plots=("fast", "bin_fast"), protocol=False)
+            self.data.plot(
+                ax2, self.current_number, plots=("fast", "bin_fast"),
+                protocol=False
+                )
             return fig
 
     def draw_fig(self):
--- langerhansGUI/view.py
+++ langerhansGUI/view.py
@@ -43,84 +43,147 @@ class View(tk.Tk):
         aboutmenu = tk.Menu(menubar, tearoff=0)
 
         menubar.add_cascade(label="Import", menu=importmenu)
-        importmenu.add_command(label="Import data", command=self.controller.import_data)
-        importmenu.add_command(label="Import settings", command=self.controller.import_settings)
-        importmenu.add_command(label="Import excluded", command=self.controller.import_excluded)
-        importmenu.add_command(label="Import object (pickle)", command=self.controller.import_object)
+        importmenu.add_command(label="Import data",
+                               command=self.controller.import_data
+                               )
+        importmenu.add_command(label="Import settings",
+                               command=self.controller.import_settings
+                               )
+        importmenu.add_command(label="Import excluded",
+                               command=self.controller.import_excluded
+                               )
+        importmenu.add_command(label="Import object (pickle)",
+                               command=self.controller.import_object
+                               )
         menubar.add_cascade(label="Export", menu=exportmenu)
-        exportmenu.add_command(label="Export settings", command=self.controller.save_settings)
-        exportmenu.add_command(label="Export image", command=self.controller.save_image)
-        exportmenu.add_command(label="Export event plot", command=self.controller.save_eventplot)
-        exportmenu.add_command(label="Export excluded", command=self.controller.save_excluded)
-        exportmenu.add_command(label="Export object (pickle)", command=self.controller.save_object)
+        exportmenu.add_command(label="Export settings",
+                               command=self.controller.save_settings
+                               )
+        exportmenu.add_command(label="Export image",
+                               command=self.controller.save_image
+                               )
+        exportmenu.add_command(label="Export event plot",
+                               command=self.controller.save_eventplot
+                               )
+        exportmenu.add_command(label="Export excluded",
+                               command=self.controller.save_excluded
+                               )
+        exportmenu.add_command(label="Export object (pickle)",
+                               command=self.controller.save_object
+                               )
         menubar.add_cascade(label="Edit", menu=editmenu)
-        editmenu.add_command(label="Settings", command=self.controller.edit_settings)
+        editmenu.add_command(label="Settings",
+                             command=self.controller.edit_settings
+                             )
         menubar.add_cascade(label="About", menu=aboutmenu)
-        aboutmenu.add_command(label="Info", command=lambda: webbrowser.open("https://github.com/janzmazek/cell-networks"))
+        aboutmenu.add_command(label="Info",
+                              command=lambda: webbrowser.open(
+                                  "https://github.com/janzmazek/cell-networks"
+                                  )
+                              )
 
         self.config(menu=menubar)
 
-        # ------------------------------ TOOLBAR ----------------------------- #
+        # ------------------------------ TOOLBAR ---------------------------- #
 
-        self.toolbar = tk.LabelFrame(self, text="Preprocessing Tools", padx=5, pady=5, bg=BG, fg=TEXT)
+        self.toolbar = tk.LabelFrame(self, text="Preprocessing Tools",
+                                     padx=5, pady=5, bg=BG, fg=TEXT
+                                     )
         self.toolbar.pack(side=tk.TOP, fill=tk.BOTH, expand=tk.NO)
 
         topframe = tk.Frame(self.toolbar, bg=BG)
         topframe.pack(anchor="center")
 
-        filter_button = tk.Button(topframe, highlightbackground=BG, text="Filter", command=self.controller.filter_click)
-        filter_button.pack(side=tk.LEFT)
+        flt_button = tk.Button(topframe, highlightbackground=BG,
+                               text="Filter",
+                               command=self.controller.filter_click
+                               )
+        flt_button.pack(side=tk.LEFT)
 
-        distributions_button = tk.Button(topframe, highlightbackground=BG, text="Compute distributions", command=lambda: self.controller.distributions_click())
-        distributions_button.pack(side=tk.LEFT)
+        dst_button = tk.Button(topframe, highlightbackground=BG,
+                               text="Compute distributions",
+                               command=self.controller.distributions_click
+                               )
+        dst_button.pack(side=tk.LEFT)
 
-        binarize_button = tk.Button(topframe, highlightbackground=BG, text="Binarize", command=lambda: self.controller.binarize_click())
-        binarize_button.pack(side=tk.LEFT)
+        bin_button = tk.Button(topframe, highlightbackground=BG,
+                               text="Binarize",
+                               command=self.controller.binarize_click
+                               )
+        bin_button.pack(side=tk.LEFT)
 
-        autoexclude_button = tk.Button(topframe, highlightbackground=BG, text="Autoexclude", command=lambda: self.controller.autoexclude_click())
-        autoexclude_button.pack(side=tk.LEFT)
+        aex_button = tk.Button(topframe, highlightbackground=BG,
+                               text="Autoexclude",
+                               command=self.controller.autoexclude_click
+                               )
+        aex_button.pack(side=tk.LEFT)
 
-        autolimit_button = tk.Button(topframe, highlightbackground=BG, text="Autolimit", command=lambda: self.controller.autolimit_click())
-        autolimit_button.pack(side=tk.LEFT)
+        alim_button = tk.Button(topframe, highlightbackground=BG,
+                                text="Autolimit",
+                                command=self.controller.autolimit_click
+                                )
+        alim_button.pack(side=tk.LEFT)
 
-        # ------------------------------ CANVAS ------------------------------ #
+        # ------------------------------ CANVAS ----------------------------- #
 
         self.canvas = tk.Canvas(self)
         self.canvas.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
 
-        middleframe = tk.Frame(self.canvas, bg=BG, borderwidth=5, relief=tk.RAISED)
+        middleframe = tk.Frame(self.canvas,
+                               bg=BG, borderwidth=5, relief=tk.RAISED
+                               )
         middleframe.pack(side=tk.TOP, fill="none", expand=True)
 
         text = tk.Label(middleframe, bg=BG, fg=WHITE, text=WELCOME_TEXT)
         text.pack(anchor="center", padx=10, pady=10)
 
-        data_button = tk.Button(middleframe, highlightbackground=BG, text="Import Data", command=self.controller.import_data)
+        data_button = tk.Button(middleframe, highlightbackground=BG,
+                                text="Import Data",
+                                command=self.controller.import_data
+                                )
         data_button.pack(side=tk.LEFT, padx=20, pady=20)
 
-        object_button = tk.Button(middleframe, highlightbackground=BG, text="Import Object", command=self.controller.import_object)
+        object_button = tk.Button(middleframe, highlightbackground=BG,
+                                  text="Import Object",
+                                  command=self.controller.import_object
+                                  )
         object_button.pack(side=tk.RIGHT, padx=20, pady=20)
-        # ------------------------------ NAVBAR ------------------------------ #
+        # ------------------------------ NAVBAR ----------------------------- #
 
-        self.navbar = tk.LabelFrame(self, text="Navigation", padx=5, pady=5, bg=BG, fg=TEXT)
+        self.navbar = tk.LabelFrame(self, text="Navigation",
+                                    padx=5, pady=5, bg=BG, fg=TEXT
+                                    )
         self.navbar.pack(side=tk.BOTTOM, fill=tk.BOTH, expand=tk.NO)
 
-        exclude_button = tk.Button(self.navbar, highlightbackground=BG, text="(↓) exclude", command=self.controller.exclude_click)
+        exclude_button = tk.Button(self.navbar, highlightbackground=BG,
+                                   text="(↓) exclude",
+                                   command=self.controller.exclude_click
+                                   )
         exclude_button.pack(side=tk.LEFT)
 
         bottomframe = tk.Frame(self.navbar, bg=BG)
         bottomframe.pack(side=tk.LEFT, fill="none", expand=True)
 
-        previous_button = tk.Button(bottomframe, highlightbackground=BG, text="(←) Prev", command=self.controller.previous_click)
-        previous_button.pack(side=tk.LEFT)
+        prev_button = tk.Button(bottomframe, highlightbackground=BG,
+                                text="(←) Prev",
+                                command=self.controller.previous_click
+                                )
+        prev_button.pack(side=tk.LEFT)
 
         self.cell_number_text = tk.Label(bottomframe, bg=BG, fg=TEXT, text="0")
         self.cell_number_text.pack(side=tk.LEFT)
 
-        next_button = tk.Button(bottomframe, highlightbackground=BG, text="Next (→)", command=self.controller.next_click)
+        next_button = tk.Button(bottomframe, highlightbackground=BG,
+                                text="Next (→)",
+                                command=self.controller.next_click
+                                )
         next_button.pack(side=tk.LEFT)
 
-        unexclude_button = tk.Button(self.navbar, highlightbackground=BG, text="unexclude (↑)", command=self.controller.unexclude_click)
-        unexclude_button.pack(side=tk.RIGHT)
+        unex_button = tk.Button(self.navbar, highlightbackground=BG,
+                                text="unexclude (↑)",
+                                command=self.controller.unexclude_click
+                                )
+        unex_button.pack(side=tk.RIGHT)
 
         self.bind("<Left>", lambda e: self.controller.previous_click())
         self.bind("<Right>", lambda e: self.controller.next_click())
@@ -129,12 +192,20 @@ class View(tk.Tk):
 
         self.minsize(width=WIDTH, height=HEIGHT)
 
-        self.progressbar = Progressbar(self, orient=tk.HORIZONTAL, length=100, mode='determinate') 
+        self.progressbar = Progressbar(self, orient=tk.HORIZONTAL,
+                                       length=100, mode='determinate'
+                                       )
         self.progressbar.pack(side=tk.BOTTOM, fill=tk.BOTH, expand=tk.NO)
 
-
     def open_file(self):
-        filename = filedialog.askopenfilename(title="Select file", filetypes=(("dat files", "*.dat"), ("YAML files", "*.yaml"), ("pickle files", "*.pkl")))
+        filename = filedialog.askopenfilename(
+            title="Select file",
+            filetypes=(
+                ("dat files", "*.dat"),
+                ("YAML files", "*.yaml"),
+                ("pickle files", "*.pkl")
+                )
+            )
         if filename == '':
             return None
         return filename
@@ -150,7 +221,9 @@ class View(tk.Tk):
         return directory
 
     def save_as(self, extension):
-        filename = filedialog.asksaveasfile(mode='w', defaultextension=extension)
+        filename = filedialog.asksaveasfile(
+            mode='w', defaultextension=extension
+            )
         if filename is None:
             return None
         return filename.name
@@ -175,8 +248,13 @@ class View(tk.Tk):
 
         self.entries = self.__add_frame(settings, main_frame)
 
-        apply_parameters_button = tk.Button(main_frame, highlightbackground=BG, text="Apply parameters", command=lambda: self.controller.apply_parameters_click())
-        apply_parameters_button.pack(side=tk.BOTTOM, fill=tk.BOTH, expand=tk.YES, padx=5, pady=5)
+        apply_parameters_button = tk.Button(
+            main_frame, highlightbackground=BG, text="Apply parameters",
+            command=self.controller.apply_parameters_click
+            )
+        apply_parameters_button.pack(
+            side=tk.BOTTOM, fill=tk.BOTH, expand=tk.YES, padx=5, pady=5
+            )
 
     def __add_frame(self, parameter, container):
         if type(parameter) in (int, float):
@@ -188,9 +266,13 @@ class View(tk.Tk):
         elif type(parameter) is dict:
             dictionary = {}
             for key in parameter:
-                parameter_frame = tk.LabelFrame(container, text=key, bg=BG, fg=TEXT)
+                parameter_frame = tk.LabelFrame(
+                    container, text=key, bg=BG, fg=TEXT
+                    )
                 parameter_frame.pack(side=tk.TOP, fill=tk.BOTH, expand=tk.YES)
-                dictionary[key] = self.__add_frame(parameter[key], parameter_frame)
+                dictionary[key] = self.__add_frame(
+                    parameter[key], parameter_frame
+                    )
             return dictionary
         elif type(parameter) is list:
             array = []
